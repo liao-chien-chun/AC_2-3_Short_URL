2916da934b062c7c19781e473f2781e102b766e5
Eric LIAO <d1044171009@gmail.com>
2022-08-01 20:14:44 +0800

    test：測試種子資料 讓他顯示在首頁

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+// 引用Shortener model
+const Shortener = require('../../models/shortener')
+
 router.get('/', (req, res) => {
-  res.render('index')
+  Shortener.find()
+    .lean()
+    .sort({ _id: 'asc' })  //排列
+    .then(shorteners => res.render('index', { shorteners }))
+    .catch(err => console.log(err))
 })
 
 module.exports = router
