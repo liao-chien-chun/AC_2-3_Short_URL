cb6dcf9b496ca8775a332ebfc1106048e03e898c
Eric LIAO <d1044171009@gmail.com>
2022-08-01 23:10:29 +0800

    feat: 創造短網址函式

---
+++ generateShorturl.js
@@ -0,0 +1,19 @@
+function generateShortUrl (URL) {
+  const myApp = 'http://localhost:3000/'
+  let newUrl = ''
+
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const uperCaseLetters = lowerCaseLetters.toUpperCase()
+  const number = '1234567890'
+  //把可能會用到的字母跟數字存在一個變數字串
+  const collection = lowerCaseLetters + uperCaseLetters + number
+  
+  for (let i = 0; i < 5; i++) {
+    newUrl += collection[Math.floor(Math.random() * collection.length)]
+  }
+  
+  const shortUrl = myApp + newUrl
+}
+
+
+
