5c6a51c624f3c6af8b7b4a97aca5e3b32a566b2e
Eric LIAO <d1044171009@gmail.com>
2022-08-04 16:25:40 +0800

    feat: 修改搜尋縮址邏輯 與 加上註解

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -18,22 +18,30 @@ router.post('/', (req, res) => {
   Shortener.findOne({ originalURL: url })
     .then(data => {
       if (data) {
+        //若網址已存在會導向已存在頁面
+        //會去資料庫找資料然後回傳一樣的縮址
         console.log('此網址已存在')
         res.render('exist', { shortURL: data.shortURL, originalURL: data.originalURL })
       } else {
+        //資料不存在則新增原本網址與創造縮址並存進資料庫
+        const shortUrl = 'http://localhost:3000/' + generateShortUrl()
         console.log('此網址不存在')
         Shortener.create({
-          originalURL: url, shortURL: 'http://localhost/' + generateShortUrl() })
-          .then(() => res.render('new', { originalURL: url, shortURL: 'http://localhost/' + generateShortUrl() }))
+          originalURL: url, shortURL: shortUrl })
+          .then(() => res.render('new', { originalURL: url, shortURL: shortUrl }))
       }
     })
 
 })
 
+
+//複製縮網址後，直接在瀏覽器搜尋的路由
+//運用params的方式取得後五碼亂碼網址
+//加上http://localhost:3000/ 存到url裡面
+//去資料庫找資料，資料存在則導向原網址
 router.get('/:shortURL', (req, res) => {
   const { shortURL } = req.params
-  const url = 'http://localhost' + shortURL
-  console.log(req.headers.host)
+  const url = 'http://localhost:3000/' + shortURL
 
   Shortener.findOne({ url })
     .then(data => {
