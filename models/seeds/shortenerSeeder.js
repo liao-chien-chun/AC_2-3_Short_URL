0aece3fcd453b03b938d773838465e490d7a2457
Eric LIAO <d1044171009@gmail.com>
2022-08-04 16:38:56 +0800

    feat: 修改種子資料

--- models/seeds/shortenerSeeder.js
+++ models/seeds/shortenerSeeder.js
@@ -6,9 +6,15 @@ const db = require('../../config/mongoose')
 //成功
 db.once('open', () => {
   console.log('add Seed data')
-  Shortener.create({
-    originalURL: 'https://www.facebook.com/meeric.tony/',
-    shortURL: 'http//localhost:3000/123AB'
-  })
+  Shortener.create(
+    {
+      originalURL: 'https://www.facebook.com/',
+      shortURL: 'http//localhost:3000/123AB'
+    },
+    {
+      originalURL: 'https://www.youtube.com/',
+      shortURL: 'http//localhost:3000/456CD'
+    }
+  )
   console.log('done')
 })
